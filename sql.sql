7aa0696731d7be17ccdb5f64b5937bc024e8a881
rajilo <richardajiloo@gmail.com>
2024-11-03 13:21:57 -0600

    added my ERD data and sql file

--- sql.sql
+++ sql.sql
@@ -1,1 +1,32 @@
-python3 -m venv my_project_env
+CREATE TABLE [Manufacturing_Process] (
+  [MachineID] TEXT(PK),
+  [ProductionBatchNumber] TEXT,
+  [OperatorID] text,
+  [Start_End_Time] DATETIME,
+  [Defect_Rate] DECIMAL
+)
+
+CREATE TABLE [Product] (
+  [ProductID] TEXT(PK),
+  [ProductionBatchNumber] TEXT(UNIQUE),
+  [ProductName] TEXT,
+  [Defect_Rate] DECIMAL
+)
+
+CREATE TABLE [Supplier] (
+  [SupplierID] TEXT(PK),
+  [MaterialType] TEXT,
+  [DeliveryDate] DATE,
+  [BatchNumber] TEXT
+)
+
+CREATE TABLE [Quality_Metric] (
+  [QCBatchID] TEXT(PK),
+  [DefectType] text,
+  [InspectionDate] DATE,
+  [BatchNumber] TEXT,
+  [ActionTaken_ScrapOrRework] TEXT
+)
+
+
+
